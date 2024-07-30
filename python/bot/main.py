5f999838c66c4b96a5589c7790bd96f670412766
Vít Listík <tivvit@seznam.cz>
2024-07-30 22:37:54 +0200

    use the bot

--- python/bot/main.py
+++ python/bot/main.py
@@ -3,11 +3,42 @@ import uw
 STEAM_PATH = "/data/games/steamapps/common/Unnatural Worlds/bin"
 
 
-def tick_callback_closure(game):
-    def tick_callback(stepping):
-        print(game.tick())
+class Bot:
+    def __init__(self, game):
+        self.game = game
+        self.step = 0
 
-    return tick_callback
+        # register update callback
+        self.game.add_update_callback(self.tick_callback_closure())
+
+    def start(self):
+        print("starting")
+        if not self.game.try_reconnect():
+            self.game.set_start_gui(True)
+            if not self.game.set_connect_find_lan():
+                self.game.connect_new_server()
+        print("done")
+
+    def attack_nearest_enemies(self):
+        pass
+
+    def assign_random_recipes(self):
+        pass
+
+    def tick_callback_closure(self):
+        def tick_callback(stepping):
+            if not stepping:
+                return
+            self.step += 1
+            print(self.game.tick())
+            print(self.step)
+            if self.step % 10 == 1:
+                self.attack_nearest_enemies()
+
+            if self.step % 10 == 5:
+                self.assign_random_recipes()
+
+        return tick_callback
 
 
 if __name__ == '__main__':
@@ -15,12 +46,18 @@ if __name__ == '__main__':
     game.log("Hello from the example bot!")
     game.log("this is a mistake", severity=uw.Severity.Error)
     game.set_player_name("tivvit")
-    game.set_player_color(255, 255, 255)
+    game.set_player_color(1, 1, 1)
     # game.connect_lobby_id(123)
     print(game.connection_state_enum())
     print(game.map_state_enum())
     print(game.tick())
-    game.add_update_callback(tick_callback_closure(game))
+
+    bot = Bot(game)
+    bot.start()
+
+    print(game.connection_state_enum())
+    print(game.map_state_enum())
+    print(game.tick())
 
     # uw_path = STEAM_PATH
     #
