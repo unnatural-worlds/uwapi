06436a1d3b52c4ab7f2c3d905ac45af8d9179968
Tomas Maly <malytomas@ucpu.cz>
2025-02-17 14:36:46 +0100

    version 27

--- csharp/uwapi/interop.cs
+++ csharp/uwapi/interop.cs
@@ -241,7 +241,7 @@ namespace Unnatural
         [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void uwCommandSelfDestruct(uint entity);
 
-        public const uint UW_VERSION = 26;
+        public const uint UW_VERSION = 27;
         public const uint UW_GameTicksPerSecond = 20;
         [StructLayout(LayoutKind.Sequential)]
         public struct UwIds
