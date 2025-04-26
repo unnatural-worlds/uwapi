6a89c7999575db2c2a274d5655f0eca658a266e9
Tomas Maly <malytomas@ucpu.cz>
2025-04-26 17:09:32 +0200

    initial sphinx docs

---
+++ sphinx/source/concepts/index.rst
@@ -0,0 +1,9 @@
+General Concepts
+================
+Glossary and explanation of concepts used throughout this documentation and in the game.
+
+.. toctree::
+	tiles
+	clusters
+	prototypes
+	ecs
---
+++ sphinx/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'uwapi'
+copyright = '2025, Tomáš Malý'
+author = 'Tomáš Malý'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.apidoc',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.githubpages',
+    'sphinx_design'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
